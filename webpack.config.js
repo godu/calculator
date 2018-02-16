6ed1a209a46cb9ab6dac42fd40f6d0eea5c4c073
godu <arthur.weber@viacesi.fr>
2018-02-16 17:31:53 +0100

    Adds production profile

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 const {join} = require('path');
+const {readFile} = require('fs');
+const {promisify} = require('util');
+const {template} = require('lodash/fp');
+const {DefinePlugin, NamedModulesPlugin, HotModuleReplacementPlugin} = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-const isProduction = process.NODE_ENV === 'production';
+const readFileP = promisify(readFile);
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const styleLoader = {
+  loader: 'style-loader',
+  options: {
+    hmr: !isProduction
+  }
+};
+const cssLoader = {
+  loader: 'css-loader',
+  options: {
+    importLoaders: 1,
+    modules: 1,
+    minimize: isProduction,
+    sourceMap: true
+  }
+};
+const postcssLoader = {
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss'
+  }
+};
 
 module.exports = {
+  devtool: isProduction ? null : 'cheap-module-source-map',
+
   entry: {
-    app: join(__dirname, 'src/index')
+    app: join(__dirname, 'src/index.js')
   },
 
   output: {
@@ -14,9 +45,25 @@ module.exports = {
   mode: isProduction ? 'production' : 'development',
 
   devServer: {
-    contentBase: join(__dirname, 'dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    before: app => {
+      app.get('/', (req, res, next) => {
+        readFileP(join(__dirname, 'src/index.html'), {encoding: 'utf8'})
+          .then(index => {
+            return template(index)(
+              Object.assign(
+                {
+                  isProduction
+                },
+                process.env
+              )
+            );
+          })
+          .then(index => res.send(index))
+          .catch(next);
+      });
+    }
   },
 
   module: {
@@ -30,23 +77,51 @@ module.exports = {
           cacheDirectory: true
         }
       },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              importLoaders: 1
-            }
+      isProduction
+        ? {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            loader: ExtractTextPlugin.extract({
+              fallback: styleLoader,
+              use: [cssLoader, postcssLoader]
+            })
           }
-        ]
-      },
+        : {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [styleLoader, cssLoader, postcssLoader]
+          },
       {
-        loader: 'file-loader'
+        test: /\.svg$/,
+        loader: 'file-loader',
+        options: {
+          name(file) {
+            if (isProduction) {
+              return '[hash].[ext]';
+            }
+
+            return '[path][name].[ext]';
+          }
+        }
       }
     ]
+  },
+  plugins: [
+    isProduction ? null : new NamedModulesPlugin(),
+    isProduction ? null : new HotModuleReplacementPlugin(),
+    new DefinePlugin(process.env),
+    isProduction
+      ? new ExtractTextPlugin({
+          filename: '[name].css'
+        })
+      : null
+  ].filter(p => p),
+
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 };
